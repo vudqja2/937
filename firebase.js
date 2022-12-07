9a67b0dd308cca4332d8aa76e3631d8b991c1ab7
937 <97590361+vudqja2@users.noreply.github.com>
2022-12-07 22:13:10 +0900

    Update and rename all.js to firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
-//firebase---------------------------------------------------------------------------------------------
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,4 +20,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-//firebase---------------------------------------------------------------------------------------------
+const database = getDatabase();
