eede73e1692347a773f85dee3cf66edaadd9b035
937 <97590361+vudqja2@users.noreply.github.com>
2024-06-13 23:06:16 +0900

    Update main.js

--- poskey/main.js
+++ poskey/main.js
@@ -1,6 +1,7 @@
 let pk = {
   toeng: function(n){
-    if(n>=1 && n<=25) return String.fromCharCode(n+64)
+    if(n>=1 && n<=13) return String.fromCharCode(n+64)
+    else if(n<=26) return String.fromCharCode(n+65)
     else return undefined
   },
   tonum: function(c){
@@ -8,28 +9,22 @@ let pk = {
     if(n>=65 && n<=90) return n-64
     else return undefined
   },
-  topos: function(n){
-    if(n>=1 && n<=26){
-      let num = n
-      if(num > 13) num--
-      let x = num%5
-      let y = Math.ceil(num/5)
-      if(!x) x=5
-      return [x,y]
-    }
-    else return undefined
+  addc: function(key){
+    document.querySelector('#'+(key=='N'?'M':key)).classList.add('active')
   },
-  mov: function(x,y){
-    let chr =  document.querySelector('#chr')
-    chr.style.setProperty('--x', x.toString())
-    chr.style.setProperty('--y', y.toString())
-  },
+  delc: function(key){
+    document.querySelector('#'+(key=='N'?'M':key)).classList.remove('active')
+  }
 }
 document.addEventListener('keydown', e => {
   key = e.key.toUpperCase()
-  console.log(key)
   if(key.length==1 && pk.tonum(key)){
-    [x,y]=pk.topos(pk.tonum(key))
-    pk.mov(x,y)
+    pk.addc(key)
+  }
+})
+document.addEventListener('keyup', e => {
+  key = e.key.toUpperCase()
+  if(key.length==1 && pk.tonum(key)){
+    pk.delc(key)
   }
 })
