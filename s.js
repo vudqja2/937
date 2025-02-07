74333d653c1d2095aa3d0e168a07280848c413c6
937 <97590361+vudqja2@users.noreply.github.com>
2025-02-07 17:19:33 +0900

    Update s.js

--- s.js
+++ s.js
@@ -1,5 +1,4 @@
-document.body.innerHTML = '';
+new Promise((res, rej) => {document.body.innerHTML = '';})
+.catch(e => console.log("This site doesn't allow you to remove elements."));
 let n = 1;
-setInterval(()=>{
-    document.body.style.backgroundColor = (n*=-1)==1?'#000':'#fff';
-},20)
+setInterval(()=>{document.body.style.backgroundColor = (n*=-1)==1?'#000':'#fff';},20)
